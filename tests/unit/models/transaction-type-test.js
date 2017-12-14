f367dff7966e55dba2472c73cace170ed657d787
Justin Drew <jdrew@sca-corp.com>
2017-12-13 19:18:39 -0700

    finish model unit tests

--- tests/unit/models/transaction-type-test.js
+++ tests/unit/models/transaction-type-test.js
@@ -1,4 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
+import runAttrTest from 'finance-app/tests/helpers/attribute-test';
+import runRelationshipTest from 'finance-app/tests/helpers/relationship-test';
 
 moduleForModel('transaction-type', 'Unit | Model | transaction type', {
   // Specify the other units that are required for this test.
@@ -9,4 +11,30 @@ test('it exists', function(assert) {
   const model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+// attribute tests
+test('createdAt is a date attribute', function(assert) {
+  runAttrTest.call(this, assert, 'transaction-type', 'createdAt', 'date', true, new Date());
+});
+
+test('isDebit is a boolean attribute', function(assert) {
+  runAttrTest.call(this, assert, 'transaction-type', 'isDebit', 'boolean', false, true);
+});
+
+test('name is a string attribute', function(assert) {
+  runAttrTest.call(this, assert, 'transaction-type', 'name', 'string', false, 'test');
+});
+
+test('updatedAt is a date attribute', function(assert) {
+  runAttrTest.call(this, assert, 'transaction-type', 'updatedAt', 'date', true, new Date());
+});
+
+// relationship tests
+test('should have many audits', function(assert) {
+  runRelationshipTest.call(this, assert, 'transaction-type', 'audits', 'hasMany', 'audit', 'auditable', false, false);
+});
+
+test('should have many transactions', function(assert) {
+  runRelationshipTest.call(this, assert, 'transaction-type', 'transactions', 'hasMany', 'transaction', null, false, false);
 });
--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -1,4 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
+import runAttrTest from 'finance-app/tests/helpers/attribute-test';
+import runRelationshipTest from 'finance-app/tests/helpers/relationship-test';
 
 moduleForModel('user', 'Unit | Model | user', {
   // Specify the other units that are required for this test.
@@ -9,4 +11,22 @@ test('it exists', function(assert) {
   const model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+// attribute tests
+test('password is a string attribute', function(assert) {
+  runAttrTest.call(this, assert, 'user', 'password', 'string', false, 'test');
+});
+
+test('username is a string attribute', function(assert) {
+  runAttrTest.call(this, assert, 'user', 'username', 'string', true, 'test');
+});
+
+// relationship tests
+test('should have many audits', function(assert) {
+  runRelationshipTest.call(this, assert, 'user', 'audits', 'hasMany', 'audit', 'auditable', false, false);
+});
+
+test('should have a profile', function(assert) {
+  runRelationshipTest.call(this, assert, 'user', 'profile', 'belongsTo', 'profile', null, false, false);
 });
