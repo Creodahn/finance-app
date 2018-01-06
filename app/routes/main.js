61cbecf365b5ba86ceba9868fcc545fbf798cf1b
Justin Drew <jdrew@sca-corp.com>
2018-01-06 15:07:31 -0700

    styling upgrades for lists; simplify some dumb things

--- app/routes/main.js
+++ app/routes/main.js
@@ -2,12 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    let model = null;
-
-    if(this.get('session.isAuthenticated')) {
-      model = this.get('sessionAccount.profile');
-    }
-
-    return model;
+    return this.get('sessionAccount.profile');
   }
 });
